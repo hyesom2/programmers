029c8bdd62a6f0433257dd6783320537dc45cabd
HyeonJu <123542438+hyesom2@users.noreply.github.com>
2025-03-19 00:01:27 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    const english = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    
+    for(let i = 0; i < english.length; i++) {
+        numbers = numbers.replaceAll(english[i], i);
+    }
+    
+    return Number(numbers);
+}
